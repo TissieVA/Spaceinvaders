bebedd27e9c89f5ef31953c11bae195203c8c081
Tijs Van Alphen <tijs.van.alphen@hotmail.com>
2020-04-27 15:54:50 +0200

    creating bonusship

---
+++ GameObject/BonusShip.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tijs Van Alphen on 27/04/2020.
+//
+
+#include "BonusShip.h"
---
+++ GameObject/BonusShip.h
@@ -0,0 +1,15 @@
+//
+// Created by Tijs Van Alphen on 27/04/2020.
+//
+
+#ifndef GAME_BONUSSHIP_H
+#define GAME_BONUSSHIP_H
+
+
+class BonusShip
+{
+
+};
+
+
+#endif //GAME_BONUSSHIP_H
